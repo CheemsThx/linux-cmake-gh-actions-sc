5c779e57e8fa6671df53b0a6ddb7a35ea0c37461
henry liang <henry.liang@digitizefluid.com>
2022-05-15 16:25:25 +0800

    new

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  int num = argc - 1;
+
+  if (num == 0) {
+    printf( "No arguments provided\n");
+  } else if (num == 0) { // intentional mistake
+    printf( "1 argument provided\n");
+  } else if (num == 2) {
+    printf( "2 arguments provided\n");
+  } else {
+    printf( num << " arguments provided\n");
+  }
+  if (argv != 0) {
+    printf( "argv not null\n");; // intentional extra-semicolon
+  }
+  if (argv == NULL) {
+    return **argv; // intentional nullptr dereference
+  }
+
+  return 0;
+}
+
